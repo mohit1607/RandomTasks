0c022a20519caf00b883f3968e4d09736e97a1c6
Mohit Ashiliya <mohit.ashliya@gmail.com>
2021-09-01 19:13:27 +0530

    first  commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import { useState , useEffect} from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+
+  const [currData, setCurrData] = useState("");
+  const [Data, setData] = useState([]);            //Hooks are pretty simpler
+  const [click,setClick] = useState(false);
+
+
+
+ 
+
+  const add = () => {
+    
+    setClick(!click);
+    
+  }
+
+  useEffect(() => {
+    const getData = async()=> {
+      try{
+       const res = await fetch("https://www.boredapi.com/api/activity");
+       const data = await res.json();
+       console.log(data.activity);
+       setCurrData(data.activity);
+      }catch(err){
+        console.log(err);
+      }
+     };
+    
+     getData();
+
+     setData((old) =>{
+      return [...old,  currData];
+    })
+    console.log(Data)
+     
+
+  }, [click])
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+    <h3>Random activity if you  are Bored</h3>
+    {
+      Data.map(
+      (curr,ind) => {
+        if(ind >= 1){
+          return <h4>{`${ind}-->${curr}`}</h4>
+        }
+      }
+    )
+    }
+    
     </div>
+
+    <div className="btn">
+      <button className="b" onClick ={add}>Click for suggestions</button>
+    </div>
+    </>
   );
 }
 
